f3cccefcc6eae1455577793d7a38e926a5b208b6
Dane Finlay <Danesprite@gmail.com>
2017-09-30 23:35:42 +1000

    Make assert_callable_arg_count raise NotImplementedError for Python 3+ as it doesn't work for those versions yet without a lot of rework

--- pyutil.c
+++ pyutil.c
@@ -9,17 +9,21 @@
 
 bool
 assert_callable_arg_count(PyObject *value, const unsigned int arg_count) {
+#if PY_MAJOR_VERSION >= 3
+    // This doesn't work in Python 3+. See header.
+    PyErr_Format(PyExc_NotImplementedError,
+		 "internal C function '%s' is not yet "
+		 "implemented for this version of Python.",
+		 __func__);
+    return false;
+#else
     int count = -1;
     bool result = true;
     PyObject* fc = PyObject_GetAttrString(value, "func_code");
     if(fc) {
 	PyObject* ac = PyObject_GetAttrString(fc, "co_argcount");
 	if(ac) {
-#if PY_MAJOR_VERSION >= 3
-	    count = PyLong_AsLong(ac);
-#else
 	    count = PyInt_AsLong(ac);
-#endif
 	    Py_DECREF(ac);
 	}
 	Py_DECREF(fc);
@@ -30,11 +34,12 @@ assert_callable_arg_count(PyObject *value, const unsigned int arg_count) {
     else arg_or_args = "arguments";
 
     if (count != arg_count) {
-	PyErr_Format(PyExc_TypeError, "callable must have %d %s.",
-		     arg_count, arg_or_args);
+	PyErr_Format(PyExc_TypeError, "callable must have %d %s, found %d %s",
+		     arg_count, arg_or_args, count, arg_or_args);
 	result = false;
     }
     
     return result;
+#endif
 }
 
--- pyutil.h
+++ pyutil.h
@@ -25,6 +25,11 @@
 /* Checks the argument count of a callable Python object and returns true
  * if it matches arg_count, or returns false if it doesn't and sets a 
  * Python exception.
+ *
+ * For Python 3 and above, this method returns false with a Python
+ * NotImplementedError set; the attributes it depends on are not present in
+ * Python 3+. It can be done differently for both Python 2 and 3 using Python's
+ * built-in inspect module, however I'm not sure that module can be used from C.
  */
 bool
 assert_callable_arg_count(PyObject *value, const unsigned int arg_count);
