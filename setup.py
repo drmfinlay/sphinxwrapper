24d13d491acd64753e6d353856f68d23a40594f3
Dane Finlay <Danesprite@gmail.com>
2018-01-01 00:25:20 +1100

    Update setup.py to use setuptools and the pure Python sphinxwrapper package instead

--- setup.py
+++ setup.py
@@ -1,32 +1,10 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('sphinxwrapper',
-                    sources=[
-                        'sphinxwrapper.c',
-                        'pypocketsphinx.c',
-                        'audio.c',
-                        'pyutil.c'
-                    ],
-                    include_dirs=[
-                        'include',
-                        '/usr/local/include',
-                        '/usr/local/include/sphinxbase',
-                        '/usr/local/include/pocketsphinx',
-                        '/usr/include',
-                        '/usr/include/sphinxbase',
-                        '/usr/include/pocketsphinx'
-
-                    ],
-                    libraries=[
-                        'pocketsphinx',
-                        'sphinxbase',
-                        'sphinxad'
-                    ],
-                    library_dirs=['/usr/local/lib']
-                    )
+from setuptools import setup
 
 setup(name='sphinxwrapper',
-      version='1.1',
-      description='Python C extension for Pocket Sphinx and '
-                  'other CMU Sphinx libraries',
-      ext_modules=[module1])
+      version='1.0',
+      description='Simplified Python API for using Pocket Sphinx with pyaudio',
+      author='Dane Finlay',
+      author_email='Danesprite@gmail.com',
+      packages=['sphinxwrapper'],
+      install_requires=['pyaudio', 'pocketsphinx']
+      )
