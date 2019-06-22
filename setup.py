a99824439d41796b99ef18d750f94885a48e6fad
Dane Finlay <Danesprite@posteo.net>
2019-06-22 23:22:04 +1000

    Update setup.py
    - Add classifiers, license name and URL.
    - Change short description.
    - Include README.rst as the project's long description.
    - Update email address.

--- setup.py
+++ setup.py
@@ -1,10 +1,33 @@
 from setuptools import setup
 
-setup(name='sphinxwrapper',
-      version='1.1.1',
-      description='Simplified Python API for using Pocket Sphinx with pyaudio',
-      author='Dane Finlay',
-      author_email='Danesprite@gmail.com',
-      packages=['sphinxwrapper'],
-      install_requires=['pocketsphinx']
-      )
+
+def get_long_description():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='sphinxwrapper',
+    version='1.1.1',
+    description='Alternative Python API for recognising speech with CMU '
+                'Pocket Sphinx',
+    long_description=get_long_description(),
+    url='https://github.com/Danesprite/sphinxwrapper',
+    author='Dane Finlay',
+    author_email='Danesprite@posteo.net',
+    license='MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Topic :: Multimedia :: Sound/Audio :: Speech',
+        'Topic :: Software Development :: Libraries',
+    ],
+    packages=['sphinxwrapper'],
+    install_requires=['pocketsphinx']
+)
